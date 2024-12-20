ed76624ec8753dde3a5b39277560e5eb636665d0
NV310796 <geniynashihdney@gmail.com>
2024-12-20 22:06:46 +0300

    Add files via upload

---
+++ modul_1_6.py
@@ -0,0 +1,5 @@
+the_number_of_finished_works=12
+time_spent=1.5
+course_name='Python'
+time_for_one_task=time_spent/the_number_of_finished_works
+print('Курс: ', course_name, ', всего задач: ', the_number_of_finished_works, ', затрачено часов: ', time_spent, ', среднее время выполнения: ', time_for_one_task, 'часа.',sep='')
